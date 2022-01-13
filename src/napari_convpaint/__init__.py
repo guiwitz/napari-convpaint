78034430f2893f51dbfb05d97c3ab633606c100f
guiwitz <guiwitz@users.noreply.github.com>
2022-01-13 12:03:06 +0100

    convert to npe2

--- src/napari_convpaint/__init__.py
+++ src/napari_convpaint/__init__.py
@@ -6,9 +6,3 @@ except ImportError:
 
 
 from .conv_paint import ConvPaintWidget
-from napari_plugin_engine import napari_hook_implementation
-
-@napari_hook_implementation
-def napari_experimental_provide_dock_widget():
-    # you can return either a single widget, or a sequence of widgets
-    return [ConvPaintWidget]
