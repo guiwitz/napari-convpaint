4170c323c0701bc46b8bbded357b8776b98f238c
guiwitz <guiwitz@users.noreply.github.com>
2023-07-05 11:03:15 +0200

    add label sample for single plane cell3d image

---
+++ src/napari_convpaint/convpaint_sample.py
@@ -0,0 +1,18 @@
+import tifffile
+import napari.types
+from skimage.draw import disk
+import numpy as np
+
+def create_annotation_cell3d() -> napari.types.List[napari.types.LayerDataTuple]:
+
+    
+    shape = (256,256)
+    labels = np.zeros(shape, dtype=np.uint8)
+    rr, cc = disk((135, 36), 6, shape=shape)
+    labels[rr, cc] = 1
+    rr, cc = disk((116, 93), 6, shape=shape)
+    labels[rr, cc] = 2
+
+    return [(labels, {}, 'labels')]
+    
+    
