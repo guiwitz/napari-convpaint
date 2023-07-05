3dfbe3e4d584f1aa78d7a8be96488e2c32847bc2
guiwitz <guiwitz@users.noreply.github.com>
2023-07-05 11:04:12 +0200

    update tests

--- src/napari_convpaint/_tests/test_conv_paint.py
+++ src/napari_convpaint/_tests/test_conv_paint.py
@@ -6,16 +6,12 @@ import numpy as np
 def test_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
+    my_widget = ConvPaintWidget(viewer)
     viewer.add_image(np.random.random((100, 100)))
+    my_widget.add_annotation_layer()
 
     # create our widget, passing in the viewer
-    my_widget = ConvPaintWidget(viewer)
+    
 
-    pass
-
-    '''# call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"'''
+    assert 'annotations' in viewer.layers
+    assert 'prediction' in viewer.layers
--- src/napari_convpaint/_tests/test_conv_paint_utils.py
+++ src/napari_convpaint/_tests/test_conv_paint_utils.py
@@ -1,19 +1,38 @@
 from napari_convpaint import conv_paint_utils
+from napari_convpaint.convpaint_sample import create_annotation_cell3d
 from torch.nn.modules.container import Sequential
 import numpy as np
+import skimage
 
-def test_load_model():
-    model = conv_paint_utils.load_nn_model()
-    isinstance(model,Sequential)
+def test_hook_model():
     
-    first_layer = model.state_dict()['conv1.weight']
-    assert first_layer.shape[0] == 64, f'Number of filter expected to be 64 but found {first_layer.shape[0]}'
-    assert first_layer.shape[1] == 1, f'Expect 1 channel, found {first_layer.shape[1]}'
+    model = conv_paint_utils.Hookmodel(model_name='vgg16')
+    layers = ['features.0 Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))',
+          'features.12 Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))'
+         ]
+    model.register_hooks(selected_layers=layers)
+    
+    model.features_per_layer
+    assert isinstance(model.features_per_layer, list), "Expect list of number of features"
+    assert model.features_per_layer[0] == 64, f'Number of features expected from first layer is 64 but got {model.features_per_layer[0]}'
+    assert model.features_per_layer[1] == 256, f'Number of features expected from first layer is 256 but got {model.features_per_layer[1]}'
 
 def test_filter_image():
 
-    model = conv_paint_utils.load_nn_model()
-    image = np.random.randint(0,255,(10,10))
-    all_scales = conv_paint_utils.filter_image(image, model, scalings=[1,2])
-    assert len(all_scales) == 2
-    assert all_scales[0].shape == (1, 64, 10, 10)
+    model = conv_paint_utils.Hookmodel(model_name='vgg16')
+    layers = ['features.0 Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))',
+          'features.12 Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))'
+         ]
+    model.register_hooks(selected_layers=layers)
+
+    image = skimage.data.cells3d()
+    image = image[30,1]
+    labels = create_annotation_cell3d()[0][0]
+    image = image[60:188, 0:128]
+    labels = labels[60:188, 0:128]
+
+    features, targets = conv_paint_utils.get_features_current_layers(
+        model=model, image=image, annotations=labels, use_min_features=False)
+    assert len(features.shape) == 2, f'Expecting dataframe with 2 dims but got {len(features.shape)}'
+    assert features.shape[0] == 218, f'Expecting 218 annotated pixels but got {features.shape[0]}'
+    assert features.shape[1] == 320, f'Expecting 320 features but got {features.shape[1]}'
