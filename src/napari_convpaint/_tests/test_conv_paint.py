b01c81e6a11207921078d997a886886261fbb9d3
guiwitz <guiwitz@users.noreply.github.com>
2023-11-30 22:18:25 +0100

    fix tests, add test for multi-channel training

--- src/napari_convpaint/_tests/test_conv_paint.py
+++ src/napari_convpaint/_tests/test_conv_paint.py
@@ -11,7 +11,7 @@ def test_add_layers(make_napari_viewer, capsys):
     my_widget.add_annotation_layer()    
 
     assert 'annotations' in viewer.layers
-    assert 'prediction' in viewer.layers
+    assert 'segmentation' in viewer.layers
 
 def test_annotation_layer_dims(make_napari_viewer, capsys):
     """Check that dimensions of annotation layer match image layer"""
@@ -40,7 +40,7 @@ def test_correct_model(make_napari_viewer, capsys):
     my_widget.add_annotation_layer()
     viewer.layers['annotations'].data = im_annot
     my_widget.update_classifier()
-    assert my_widget.qcombo_model_type.currentText() == 'single_layer_vgg16_rgb', "Model type not updated correctly"
+    assert my_widget.qcombo_model_type.currentText() == 'single_layer_vgg16', "Model type not updated correctly"
 
     viewer.layers.clear()
     viewer.add_image(im[:,:,0], name='sample')
@@ -64,14 +64,42 @@ def test_rgb_prediction(make_napari_viewer, capsys):
     my_widget.update_classifier()
     my_widget.predict()
 
-    recovered = viewer.layers['prediction'].data[ground_truth==1]
+    recovered = viewer.layers['segmentation'].data[ground_truth==1]
+    precision, recall = compute_precision_recall(ground_truth, recovered)
+    
+    assert precision > 0.9, f"Precision: {precision}, too low"
+    assert recall > 0.9, f"Recall: {recall}, too low"
+
+def compute_precision_recall(ground_truth, recovered):
     tp = np.sum(recovered == 2)# / np.sum(ground_truth == 1)
     fp = np.sum(recovered == 1)#/ np.sum(ground_truth == 1)
     fn = np.sum(ground_truth == 1) - tp
     precision = tp /  (tp + fp)
     recall = tp / (tp + fn)
-    assert precision > 0.9, f"Precision: {precision}, too low"
-    assert recall > 0.9, f"Recall: {recall}, too low"
+    return precision, recall
+
+def test_multi_channel_prediction(make_napari_viewer, capsys):
+    """Check that prediction is bad when disabling multi-channel training for
+    image with signal in red channel and large noise in green channel"""
+
+    im, ground_truth = generate_synthetic_square(im_dims=(100,100), square_dims=(30,30))
+    im[:,:,1] = np.random.randint(0,100,(100,100))
+    im_annot = generate_synthetic_circle_annotation(im_dims=(100,100), circle1_xy=(19,19), circle2_xy=(56,56))
+
+    viewer = make_napari_viewer()
+    my_widget = ConvPaintWidget(viewer)
+    viewer.add_image(im)
+    my_widget.check_multi_channel_training.setChecked(False)
+    my_widget.add_annotation_layer()
+    viewer.layers['annotations'].data = im_annot
+    my_widget.update_classifier()
+    my_widget.predict()
+
+    recovered = viewer.layers['segmentation'].data[ground_truth==1]
+    precision, recall = compute_precision_recall(ground_truth, recovered)
+    
+    assert precision < 0.9, f"Precision: {precision} is too high for non multi-channel training"
+
 
 def test_save_model(make_napari_viewer, capsys):
     im, ground_truth = generate_synthetic_square(im_dims=(100,100), square_dims=(30,30))
@@ -101,7 +129,7 @@ def test_load_model(make_napari_viewer, capsys):
     my_widget.load_classifier(save_file='_tests/model_dir/test_model.joblib')
     my_widget.predict()
 
-    recovered = viewer.layers['prediction'].data[ground_truth==1]
+    recovered = viewer.layers['segmentation'].data[ground_truth==1]
     tp = np.sum(recovered == 2)# / np.sum(ground_truth == 1)
     fp = np.sum(recovered == 1)#/ np.sum(ground_truth == 1)
     fn = np.sum(ground_truth == 1) - tp
