fix issues with stacks training, simplify normalization, suppress multichannel mean option

src/napari_convpaint/_tests/test_conv_paint.py
@@ -88,14 +88,13 @@ def test_multi_channel_prediction(make_napari_viewer, capsys):
 
     viewer = make_napari_viewer()
     my_widget = ConvPaintWidget(viewer)
-    viewer.add_image(im)
-    my_widget.check_multi_channel_training.setChecked(False)
+    viewer.add_image(np.moveaxis(im,2,0))
     my_widget.add_annotation_layer()
-    viewer.layers['annotations'].data = im_annot
+    viewer.layers['annotations'].data[1,:,:] = im_annot
     my_widget.update_classifier()
     my_widget.predict()
 
-    recovered = viewer.layers['segmentation'].data[ground_truth==1]
+    recovered = viewer.layers['segmentation'].data[1][ground_truth==1]
     precision, recall = compute_precision_recall(ground_truth, recovered)
     
     assert precision < 0.9, f"Precision: {precision} is too high for non multi-channel training"

src/napari_convpaint/conv_paint.py
@@ -15,7 +15,7 @@ import yaml
 
 from napari_guitils.gui_structures import VHGroup, TabSet
 #from napari_annotation_project.project_widget import ProjectWidget 
-from .conv_paint_utils import predict_image, normalize_image
+from .conv_paint_utils import predict_image, normalize_image, compute_image_stats
 from .conv_parameters import Param
 from .conv_paint_utils import (Hookmodel, get_features_current_layers,
                                train_classifier, load_trained_classifier)
@@ -45,6 +45,8 @@ class ConvPaintWidget(QWidget):
         self.project_widget = None
         self.features_per_layer = None
         self.selected_channel = None
+        self.image_mean = None
+        self.image_std = None
 
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
@@ -205,11 +207,6 @@ class ConvPaintWidget(QWidget):
         self.check_use_cuda.setToolTip('Use GPU for training and segmentation')
         self.tabs.add_named_tab('Model', self.check_use_cuda, grid_pos=[7,1,1,1])
 
-        self.check_multi_channel_training = QCheckBox('Multi channel training')
-        self.check_multi_channel_training.setChecked(True)
-        self.check_multi_channel_training.setToolTip('If checked, extract features from each channe. Otherwise average all channels.')
-        self.tabs.add_named_tab('Model', self.check_multi_channel_training, grid_pos=[8,0,1,1])
-
         if project is True:
             self._add_project()
 
@@ -269,18 +266,10 @@ class ConvPaintWidget(QWidget):
         self.load_nnmodel_btn.clicked.connect(self._on_load_nnmodel)
         self.set_nnmodel_outputs_btn.clicked.connect(self._on_click_define_model_outputs)
 
-    '''def update_layer_list(self, event):
-        
-        keep_channel = None
-        if self.selected_channel is not None:
-            keep_channel = self.selected_channel
-        self.select_layer_widget.clear()
-        self.select_layer_widget.addItems([x.name for x in self.viewer.layers])
-        if keep_channel in [x.name for x in self.viewer.layers]:
-            self.select_layer_widget.setCurrentText(keep_channel)
-        else:
-            if self.viewer.layers:
-                self.select_layer_widget.setCurrentText(self.viewer.layers[0].name)'''
+        self.radio_multi_channel.toggled.connect(self.update_image_stats)
+        self.radio_single_channel.toggled.connect(self.update_image_stats)
+        self.radio_rgb.toggled.connect(self.update_image_stats)
+
 
     def hide_annotation(self, event=None):
         """Hide annotation layer."""
@@ -306,6 +295,7 @@ class ConvPaintWidget(QWidget):
         else:
             self.add_layers_btn.setEnabled(True)
         
+        # set radio buttons depending on selected image type
         if self.select_layer_widget.value is not None:
             if self.select_layer_widget.value.rgb:
                 self.radio_rgb.setChecked(True)
@@ -320,6 +310,19 @@ class ConvPaintWidget(QWidget):
                 self.radio_multi_channel.setEnabled(True)
                 self.radio_single_channel.setEnabled(True)
                 self.radio_single_channel.setChecked(True)
+
+            self.update_image_stats()
+
+    def update_image_stats(self):
+        # update stack stats for normalization
+        if self.check_normalize.isChecked():
+            data = self.viewer.layers[self.selected_channel].data
+            if self.viewer.layers[self.selected_channel].rgb:
+                data = np.moveaxis(data, 2, 0)
+            
+            self.image_mean, self.image_std = compute_image_stats(
+                image=data,
+                first_dim_is_channel=not self.radio_single_channel.isChecked())
 
     def reset_model(self, event=None):
 
@@ -414,40 +417,37 @@ class ConvPaintWidget(QWidget):
         self.check_use_min_features.setChecked(True)
         self._on_load_nnmodel()
 
+    def get_data_train(self):
+
+        image_stack = self.viewer.layers[self.selected_channel].data
+    
+        if self.viewer.layers[self.selected_channel].rgb:
+            image_stack = np.moveaxis(image_stack, 2, 0)
+
+        if self.check_normalize.isChecked():
+            image_stack = normalize_image(
+                image=image_stack,
+                image_mean=self.image_mean,
+                image_std=self.image_std)
+            
+        return image_stack
+
     def update_classifier(self):
         """Given a set of new annotations, update the random forest model."""
 
         unique_labels = np.unique(self.select_annotation_layer_widget.value.data)
         if (not 1 in unique_labels) | (not 2 in unique_labels):
-            raise Exception('You need annoations for foreground and background')
+            raise Exception('You need annotations for foreground and background')
         
         if self.model is None:
             if not self.check_use_custom_model.isChecked():
                 self.set_default_model()
-                '''# use 2d input for 2d images or if 3d input does not represent channels
-                # use 3d input for rgb or if 3d input is channels and has dims 3
-                if self.viewer.layers[self.selected_channel].ndim == 2:
-                    if self.viewer.layers[self.selected_channel].rgb:
-                        self.set_default_model(keep_rgb=True)
-                    else:
-                        self.set_default_model()
-                
-                else:
-                    if self.radio_multi_channel.isChecked():
-                        if self.viewer.layers[self.selected_channel].data.shape[0] != 3:
-                            raise Exception(f'your input has dimensions {self.viewer.layers[self.selected_channel].data.shape}, but the default model only works with 3 channel images')
-                        self.set_default_model(keep_rgb=True)
-                    else:
-                        self.set_default_model()
-                    self.set_default_model()'''
             else:
                 raise Exception('You have to define and load a model first')
         
         device = 'cuda' if self.check_use_cuda.isChecked() else 'cpu'
 
-        data_to_pass = self.viewer.layers[self.selected_channel].data
-        if self.viewer.layers[self.selected_channel].rgb:
-            data_to_pass = np.moveaxis(data_to_pass, 2, 0)
+        image_stack = self.get_data_train()
         
         self.viewer.window._status_bar._toggle_activity_dock(True)
         with progress(total=0) as pbr:
@@ -455,15 +455,13 @@ class ConvPaintWidget(QWidget):
             pbr.set_description(f"Training")
             features, targets = get_features_current_layers(
                 model=self.model,
-                image=data_to_pass,
+                image=image_stack,
                 annotations=self.select_annotation_layer_widget.value.data,
                 scalings=self.param.scalings,
                 order=self.spin_interpolation_order.value(),
                 use_min_features=self.check_use_min_features.isChecked(),
                 device=device,
-                normalize=self.check_normalize.isChecked(),
                 image_downsample=self.spin_downsample.value(),
-                multi_channel_training=self.check_multi_channel_training.isChecked()
             )
             self.random_forest = train_classifier(features, targets)
             self.prediction_btn.setEnabled(True)
@@ -473,7 +471,8 @@ class ConvPaintWidget(QWidget):
         self.viewer.window._status_bar._toggle_activity_dock(False)
 
     def update_classifier_on_project(self):
-        """Train classifier on all annotations in project."""
+        """Train classifier on all annotations in project.
+        !!!! Need to double-check if normalization is done correctly for projects !!!!"""
 
         if self.model is None:
             if not self.check_use_custom_model.isChecked():
@@ -495,21 +494,17 @@ class ConvPaintWidget(QWidget):
             for ind in range(num_files):
                 self.project_widget.file_list.setCurrentRow(ind)
 
-                data_to_pass = self.viewer.layers[self.selected_channel].data
-                if self.viewer.layers[self.selected_channel].rgb:
-                    data_to_pass = np.moveaxis(data_to_pass, 2, 0)
+                image_stack = self.get_data_train()
 
                 features, targets = get_features_current_layers(
                     model=self.model,
-                    image=data_to_pass,
+                    image=image_stack,
                     annotations=self.select_annotation_layer_widget.value.data,
                     scalings=self.param.scalings,
                     order=self.spin_interpolation_order.value(),
                     use_min_features=self.check_use_min_features.isChecked(),
                     device=device,
-                    normalize=self.check_normalize.isChecked(),
                     image_downsample=self.spin_downsample.value(),
-                    multi_channel_training=self.check_multi_channel_training.isChecked()
                 )
                 if features is None:
                     continue
@@ -546,36 +541,38 @@ class ConvPaintWidget(QWidget):
         
         self.viewer.window._status_bar._toggle_activity_dock(True)
         with progress(total=0) as pbr:
-            if self.check_normalize.isChecked():
-                pbr.set_description(f"Normalization")
-                image_stack = normalize_image(self.viewer.layers[self.selected_channel].data,
-                                                multi_channel_training=self.check_multi_channel_training.isChecked())
-            else:
-                image_stack = self.viewer.layers[self.selected_channel].data
+            
             pbr.set_description(f"Prediction")
             if (self.viewer.dims.ndim > 2) & (not self.radio_multi_channel.isChecked()):
                 step = self.viewer.dims.current_step[0]
-                image = image_stack[step]
+                image = self.viewer.layers[self.selected_channel].data[step]
+                if self.check_normalize.isChecked():
+                    image = normalize_image(image=image,
+                                             image_mean=self.image_mean,
+                                             image_std=self.image_std)                      
                 
                 predicted_image = predict_image(
                     image, self.model, self.random_forest, self.param.scalings,
                     order=self.spin_interpolation_order.value(),
                     use_min_features=self.check_use_min_features.isChecked(),
-                    device=device, normalize=False, #We already normalized the image
+                    device=device,
                     image_downsample=self.spin_downsample.value(),
-                    multi_channel_training=self.check_multi_channel_training.isChecked()
                 )
                 self.viewer.layers['segmentation'].data[step] = predicted_image
             else:
+                image_stack = self.viewer.layers[self.selected_channel].data
                 if self.viewer.layers[self.selected_channel].rgb:
                     image_stack = np.moveaxis(image_stack, 2, 0)
+                if self.check_normalize.isChecked():
+                    image_stack = normalize_image(image=image_stack,
+                                                image_mean=self.image_mean,
+                                                image_std=self.image_std)
                 predicted_image = predict_image(
                     image_stack, self.model, self.random_forest, self.param.scalings,
                     order=self.spin_interpolation_order.value(),
                     use_min_features=self.check_use_min_features.isChecked(),
-                    device=device, normalize=False, #We already normalized the image
+                    device=device,
                     image_downsample=self.spin_downsample.value(),
-                    multi_channel_training=self.check_multi_channel_training.isChecked()
                 )
                 self.viewer.layers['segmentation'].data = predicted_image
             self.viewer.layers['segmentation'].refresh()
@@ -601,8 +598,9 @@ class ConvPaintWidget(QWidget):
         self.viewer.window._status_bar._toggle_activity_dock(True)
 
         if self.check_normalize.isChecked():
-            image_stack = normalize_image(self.viewer.layers[self.selected_channel].data,
-                                                multi_channel_training=self.check_multi_channel_training.isChecked())
+            image_stack = normalize_image(image=self.viewer.layers[self.selected_channel].data,
+                                          image_mean=self.image_mean,
+                                          image_std=self.image_std)
         else:
             image_stack = self.viewer.layers[self.selected_channel].data      
         for step in progress(range(self.viewer.dims.nsteps[0])):
@@ -611,9 +609,9 @@ class ConvPaintWidget(QWidget):
                 image, self.model, self.random_forest, self.param.scalings,
                 order=self.spin_interpolation_order.value(),
                 use_min_features=self.check_use_min_features.isChecked(),
-                device=device, normalize=False, # We already normalized above
-                image_downsample=self.spin_downsample.value(),
-                multi_channel_training=self.check_multi_channel_training.isChecked())
+                device=device,
+                image_downsample=self.spin_downsample.value()
+            )
             self.viewer.layers['segmentation'].data[step] = predicted_image
         self.viewer.window._status_bar._toggle_activity_dock(False)
 
@@ -660,7 +658,6 @@ class ConvPaintWidget(QWidget):
         self.param.use_min_features = self.check_use_min_features.isChecked()
         self.param.image_downsample = self.spin_downsample.value()
         self.param.normalize = self.check_normalize.isChecked()
-        self.param.multi_channel_training = self.check_multi_channel_training.isChecked()
 
     
     def load_classifier(self, event=None, save_file=None):
@@ -694,4 +691,3 @@ class ConvPaintWidget(QWidget):
         self.check_use_min_features.setChecked(self.param.use_min_features)
         self.spin_downsample.setValue(self.param.image_downsample)
         self.check_normalize.setChecked(self.param.normalize)
-        self.check_multi_channel_training.setChecked(self.param.multi_channel_training)

src/napari_convpaint/conv_paint_utils.py
@@ -26,7 +26,7 @@ from .conv_parameters import Param
 
 
 
-def normalize_image(image, multi_channel_training):
+def normalize_image(image, image_mean, image_std):
     """
     Normalize a numpy array with 2-4 dimensions.
 
@@ -38,13 +38,19 @@ def normalize_image(image, multi_channel_training):
     ----------
     image : np.ndarray
         Input array to be normalized. Must have 2-4 dimensions.
-    multi_channel_training : bool
-        Flag to determine whether the third dimension represents channels (True) or time/z (False).
-
+    image_mean : float or nd.array
+        Mean of the input array along non-channel dimensions. If None, the mean is calculated from the input array.
+    image_std : float or nd.array
+        Standard deviation of the input array along non-channel dimensions. If None, the standard deviation is calculated from the input array.
+        
     Returns
     -------
     arr : np.ndarray
         Normalized array.
+    image_mean : float or nd.array
+        Mean of the input array along non-channel dimensions
+    image_std : float or nd.array
+        Standard deviation of the input array along non-channel dimensions
 
     Raises
     ------
@@ -55,18 +61,55 @@ def normalize_image(image, multi_channel_training):
     if image.ndim < 2 or image.ndim > 4:
         raise ValueError("Array must have 2-4 dimensions")
 
-    # Normalize each channel independently
-    if image.ndim == 4:  # If array has 4 dimensions, assume it's (t/z, y, x, c)
-        arr_norm = (image - image.mean(axis=(0, 1, 2), keepdims=True)) / image.std(axis=(0, 1, 2), keepdims=True)
-    elif image.ndim == 3:  # If array has 3 dimensions
-        if multi_channel_training:  # If third dimension represents channels
-            arr_norm = (image - image.mean(axis=(0, 1), keepdims=True)) / image.std(axis=(0, 1), keepdims=True)
-        else:  # If third dimension represents time/z
-            arr_norm = (image - image.mean()) / image.std()
-    else:  # If array has 2 dimensions, assume it's (y, x)
-        arr_norm = (image - image.mean()) / image.std()
+    # The computing of stats and image normalization is split into two function. This will
+    # allow to normalize even a single image of the stack with the same stats as the whole stack.
+    # This is done in the perspective of handling large stacks that cannot be loaded in memory.
+    # This needs to be implemented:
+        # - computing stats stack by stack and combining the result at the end
+        # - allow to normalize a single image from a stack
+    arr_norm = (image - image_mean) / image_std
 
     return arr_norm
+
+def compute_image_stats(image, first_dim_is_channel=False):
+    """
+    Compute mean and standard deviation of a numpy array with 2-4 dimensions.
+
+    If the array has multiple channels (determined by the first_dim_is_channel option), 
+    each channel is normalized independently. Channels can only be the first dimension of the array.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        Input array to be normalized. Must have 2-4 dimensions.
+    first_dim_is_channel : bool
+        Option to specify whether first dimension of array with ndim>2 represents channels (True) or time/z (False).
+        
+    Returns
+    -------
+    image_mean : float or nd.array
+        Mean of the input array along non-channel dimensions
+    image_std : float or nd.array
+        Standard deviation of the input array along non-channel dimensions
+
+    Raises
+    ------
+    ValueError
+        If the input array does not have 2-4 dimensions.
+    """
+    
+    if image.ndim < 2 or image.ndim > 4:
+        raise ValueError("Array must have 2-4 dimensions")
+    
+    if (image.ndim == 2) | (not first_dim_is_channel):
+        image_mean = image.mean()
+        image_std = image.std()
+    else:
+        image_mean = image.mean(axis=tuple(range(1,image.ndim)), keepdims=True)
+        image_std = image.std(axis=tuple(range(1,image.ndim)), keepdims=True)
+
+    return image_mean, image_std
+
 
 def filter_image(image, model, scalings):
     """
@@ -109,7 +152,7 @@ def filter_image(image, model, scalings):
 
 
 def filter_image_multioutputs(image, hookmodel, scalings=[1], order=0, device='cpu',
-                              normalize=False, image_downsample=1):
+                              image_downsample=1):
     """Recover the outputs of chosen layers of a pytorch model. Layers and model are
     specified in the hookmodel object.
     
@@ -126,8 +169,6 @@ def filter_image_multioutputs(image, hookmodel, scalings=[1], order=0, device='c
         by default 0
     device : str, optional
         Device to use for computation, by default 'cpu'
-    normalize : bool, optional
-        If True, normalize each channel with its mean and std, by default False
     image_downsample : int, optional
         Downsample image by this factor before extracting features, by default 1
 
@@ -154,9 +195,6 @@ def filter_image_multioutputs(image, hookmodel, scalings=[1], order=0, device='c
             warnings.warn(f'Image has {image.shape[0]} channels, model expects {input_channels}. Using mean projection.')
             image = np.mean(image, axis=0)
             image = np.ones((input_channels, image.shape[0], image.shape[1]), dtype=np.float32) * image
-
-    if normalize:
-        image = (image - image.mean(axis=(1, 2), keepdims=True)) / image.std(axis=(1, 2), keepdims=True)
 
     int_mode = 'bilinear' if order > 0 else 'nearest'
     align_corners = False if order > 0 else None
@@ -193,7 +231,7 @@ def filter_image_multioutputs(image, hookmodel, scalings=[1], order=0, device='c
 
 
 def filter_image_multichannels(image, hookmodel, scalings=[1], order=0, device='cpu',
-                              normalize=False, image_downsample=1):
+                              image_downsample=1):
     """Recover the outputs of chosen layers of a pytorch model. Layers and model are
     specified in the hookmodel object. If image has multiple channels, each channel
     is processed separately.
@@ -211,8 +249,6 @@ def filter_image_multichannels(image, hookmodel, scalings=[1], order=0, device='
         by default 0
     device : str, optional
         Device to use for computation, by default 'cpu'
-    normalize : bool, optional
-        If True, normalize each channel with its mean and std, by default False
     image_downsample : int, optional
         Downsample image by this factor before extracting features, by default 1
 
@@ -236,9 +272,6 @@ def filter_image_multichannels(image, hookmodel, scalings=[1], order=0, device='
     elif image.ndim == 3:
         image = image[:, ::image_downsample, ::image_downsample]
         image_series = [np.ones((input_channels, im.shape[0], im.shape[1]), dtype=np.float32) * im for im in image]
-
-    if normalize:
-        image_series = [(im - im.mean(axis=(1, 2), keepdims=True)) / im.std(axis=(1, 2), keepdims=True) for im in image_series]
 
     int_mode = 'bilinear' if order > 0 else 'nearest'
     align_corners = False if order > 0 else None
@@ -276,8 +309,8 @@ def filter_image_multichannels(image, hookmodel, scalings=[1], order=0, device='
 
 
 def predict_image(image, model, classifier, scalings=[1], order=0,
-                  use_min_features=True, device='cpu', normalize=False, image_downsample=1,
-                  multi_channel_training=False):
+                  use_min_features=True, device='cpu', image_downsample=1,
+                  ):
     """
     Given a filter model and a classifier, predict the class of 
     each pixel in an image.
@@ -298,12 +331,8 @@ def predict_image(image, model, classifier, scalings=[1], order=0,
         if True, use the minimum number of features per layer
     device: str, optional
         device to use for computation, by default 'cpu'
-    normalize: bool, optional
-        if True, normalize each channel with its mean and std, by default False
     image_downsample: int, optional
         downsample image by this factor before extracting features, by default 1
-    multi_channel_training: bool, optional
-        if True, extract features from each channel separately, otherwise average all channels
 
     Returns
     -------
@@ -312,16 +341,13 @@ def predict_image(image, model, classifier, scalings=[1], order=0,
 
     """
     
-    if multi_channel_training:
-        filter_fun = filter_image_multichannels
-    else:
-        filter_fun = filter_image_multioutputs
+    filter_fun = filter_image_multichannels
 
     if use_min_features:
         max_features = np.min(model.features_per_layer)
         all_scales = filter_fun(
             image, model, scalings=scalings, order=order,
-            device=device, normalize=normalize, image_downsample=image_downsample)
+            device=device, image_downsample=image_downsample)
         all_scales = [a[:, 0:max_features, :, :] for a in all_scales]
         tot_filters = max_features * len(all_scales)
 
@@ -329,7 +355,7 @@ def predict_image(image, model, classifier, scalings=[1], order=0,
         # max_features = np.max(model.features_per_layer)
         all_scales = filter_fun(
             image, model, scalings=scalings, order=order,
-            device=device, normalize=normalize, image_downsample=image_downsample)
+            device=device, image_downsample=image_downsample)
         tot_filters = np.sum(a.shape[1] for a in all_scales)
     
     tot_filters = int(tot_filters)
@@ -388,7 +414,7 @@ def load_single_layer_vgg16(keep_rgb=False):
 
 def get_multiscale_features(model, image, annotations, scalings, order=0,
                             use_min_features=True, device='cpu',
-                            normalize=False, image_downsample=1, multi_channel_training=False):
+                            image_downsample=1):
     """Given an image and a set of annotations, extract multiscale features
     
     Parameters
@@ -407,12 +433,8 @@ def get_multiscale_features(model, image, annotations, scalings, order=0,
         Use minimal number of features, by default True
     device : str, optional
         Device to use for computation, by default 'cpu'
-    normalize : bool, optional
-        If True, normalize each channel with its mean and std, by default False
     image_downsample : int, optional
         Downsample image by this factor before extracting features, by default 1
-    multi_channel_training: bool, optional
-        if True, extract features from each channel separately, otherwise average all channels
 
     Returns
     -------
@@ -420,10 +442,7 @@ def get_multiscale_features(model, image, annotations, scalings, order=0,
         Extracted features. Dimensions npixels x nfeatures * nbscales
     """
 
-    if multi_channel_training:
-        filter_fun = filter_image_multichannels
-    else:
-        filter_fun = filter_image_multioutputs
+    filter_fun = filter_image_multichannels
 
     if use_min_features:
         max_features = np.min(model.features_per_layer)
@@ -437,7 +456,7 @@ def get_multiscale_features(model, image, annotations, scalings, order=0,
 
     all_scales = filter_fun(
         image, model, scalings, order=order, device=device,
-        normalize=normalize, image_downsample=image_downsample)
+        image_downsample=image_downsample)
     if use_min_features:
         all_scales = [a[:, 0:max_features, :, :] for a in all_scales]
     all_values_scales = []
@@ -454,7 +473,7 @@ def get_multiscale_features(model, image, annotations, scalings, order=0,
 
 def get_features_current_layers(model, image, annotations, scalings=[1],
                                 order=0, use_min_features=True, device='cpu',
-                                normalize=False, image_downsample=1, multi_channel_training=False):
+                                image_downsample=1, multi_channel_training=False):
     """Given a potentially multidimensional image and a set of annotations,
     extract multiscale features from multiple layers of a model.
     
@@ -475,12 +494,8 @@ def get_features_current_layers(model, image, annotations, scalings=[1],
         Use minimal number of features, by default True
     device : str, optional
         Device to use for computation, by default 'cpu'
-    normalize : bool, optional
-        If True, normalize each channel with its mean and std, by default False
     image_downsample : int, optional
         Downsample image by this factor before extracting features, by default 1
-    multi_channel_training: bool, optional
-        if True, extract features from each channel separately, otherwise average all channels
 
     Returns
     -------
@@ -504,9 +519,6 @@ def get_features_current_layers(model, image, annotations, scalings=[1],
     all_values = []
     all_targets = []
 
-    if normalize:
-        image = normalize_image(image, multi_channel_training=multi_channel_training)
-
     # iterating over non_empty iteraties of t/z for 3D data
 
     for ind, t in enumerate(non_empty):
@@ -521,8 +533,8 @@ def get_features_current_layers(model, image, annotations, scalings=[1],
         extracted_features = get_multiscale_features(
             model, current_image, current_annot, scalings,
             order=order, use_min_features=use_min_features,
-            device=device, normalize=False, # we already normalized above
-            image_downsample=image_downsample, multi_channel_training=multi_channel_training)
+            device=device,
+            image_downsample=image_downsample)
         all_values.append(extracted_features)
         
         all_targets.append(current_annot[::image_downsample, ::image_downsample]
@@ -1238,7 +1250,8 @@ class Classifier():
             im_out = np.zeros(image.shape, dtype=np.uint8)
 
         if self.param.normalize:
-            image = normalize_image(image)
+            image_mean, image_std = compute_image_stats(image)
+            image = normalize_image(image, image_mean, image_std)
 
         for i in range(image.shape[0]):
             im_out[i] = predict_image(
@@ -1249,7 +1262,6 @@ class Classifier():
                 order=self.param.order,
                 use_min_features=self.param.use_min_features,
                 device='cpu',
-                normalize=False, #we already normalized above
                 image_downsample=self.param.image_downsample)
 
         if save_path is None: