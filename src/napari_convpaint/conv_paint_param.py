6961e3be2018d6246d46535b79931f976817bc5e
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2024-11-29 11:43:32 +0100

    add param descriptions to docstring of param class

--- src/napari_convpaint/conv_paint_param.py
+++ src/napari_convpaint/conv_paint_param.py
@@ -8,6 +8,44 @@ import yaml
 class Param:
     """Object storing relevant information regarding the processing,
     e.g. the window size (padding), the analyzed data, the type of segmentation used.
+
+    Parameters saved in a Param object:
+    ----------------
+        classifier: str
+            path to the classifier model
+        multi_channel_img: bool = None
+            if the image dimensions allow, use multichannel NOTE: Needs overthinking
+        normalize: int
+            normalization mode
+            1: no normalization, 2: normalize stack, 3: normalize each image
+        image_downsample: int
+            factor for downscaling the image right after input
+            (predicted classes are upsampled accordingly for output)
+        tile_annotations: bool
+            if True, extract only features of bounding boxes around annotated areas
+        tile_image: bool
+            if True, extract features in tiles (for large images)
+        fe_name: str
+            name of the feature extractor model
+        fe_layers: list[str]
+            list of layers (names) to extract features from
+        fe_padding : int
+            padding for the feature extractor NOTE: Needs overthinking
+        fe_scalings: list[int]
+            list of scaling factors for the feature extractor, creating a pyramid of features
+            (features are upscaled accordingly before input to classifier)
+        fe_order: int
+            interpolation order used for the upscaling of features for the pyramid
+        fe_use_min_features: bool
+            if True, use the minimum number of features among all layers
+        fe_use_cuda: bool
+            whether to use cuda (GPU) for feature extraction
+        clf_iterations: int
+            number of iterations for the classifier
+        clf_learning_rate: float
+            learning rate for the classifier
+        clf_depth: int = None
+            depth of the classifier
     """
 
     classifier: str = None
